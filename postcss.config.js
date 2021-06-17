3382ff89c570159428702968bc166ddc390a960d
Lenic <Lenic@live.cn>
2021-06-17 15:04:59 +0800

    更新移除不用的 CSS 设置
    - 移除全局 CSS 移除设置，只启用了移除不用的 Tailwindcss 配置
    - 修复 border-color 使用 CSS 变量编译的问题
    
    Signed-off-by: Lenic <Lenic@live.cn>

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,8 @@
-const purgecss = require("@fullhuman/postcss-purgecss");
-
 module.exports = {
   plugins: [
-    purgecss({
-      content: ["./src/**/*.html", "./src/**/*.vue", "./src/**/*.js"],
-    }),
-    require("postcss-import"),
-    require("tailwindcss"),
-    require("postcss-nested"), // or require('postcss-nesting')
-    require("autoprefixer"),
+    require('postcss-import'),
+    require('tailwindcss'),
+    require('postcss-nested'), // or require('postcss-nesting')
+    require('autoprefixer'),
   ],
 };
--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <template>
   <div id="app" class="p-24 bg-primary px-12! bg-opacity-30 w-full h-full">
     <h1
-      class="text-warning text-36 bg-primary-light mb-12 rounded-8 leading-60 hover:text-opacity-50 hover:scale-110 transform transition-transform duration-100 cursor-pointer"
+      class="
+        text-warning text-36
+        bg-primary-light
+        mb-12
+        rounded-8
+        leading-60
+        hover:text-opacity-50
+        hover:scale-110
+        transform
+        transition-transform
+        duration-100
+        cursor-pointer
+      "
     >
       This is an about page
     </h1>
-    <div class="btn-abc">abc</div>
+    <div class="btn-abc border">abc</div>
   </div>
 </template>
 
